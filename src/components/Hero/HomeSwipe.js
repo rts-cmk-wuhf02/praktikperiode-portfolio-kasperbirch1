1c1c712f5b62e6c117ed947b62c4ee9fdc6fb67d
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-09 14:33:04 +0200

    updated

--- src/components/Hero/HomeSwipe.js
+++ src/components/Hero/HomeSwipe.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { TiArrowForwardOutline, TiArrowRightOutline } from 'react-icons/ti'
+import { TiArrowForwardOutline } from 'react-icons/ti'
 import styled from 'styled-components'
 const StyledHomeSwipe = styled.article`
 padding: 2rem;
 h2 {
     font-weight: bold;
     font-size: 2rem;
-    margin-bottom: 2rem;
+}
+h3 {
+    margin-top: 3rem;
+    font-weight: bold;
+    font-size: 1.5rem;
 }
 svg {
-    margin-top: 3rem;
     font-size: 3rem;
     transform: rotateX(180deg);
 }
@@ -20,7 +23,10 @@ const HomeSwipe = () => {
         <StyledHomeSwipe>
             <h2>Kasper Birch</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima non perspiciatis sequi, amet facere quisquam quos deserunt ducimus, voluptates magni! Nesciunt unde atque totam quasi quas nisi adipisci rem?</p>
-            <TiArrowForwardOutline />
+            <div>
+                <h3>Swipe for skils</h3>
+                <TiArrowForwardOutline />
+            </div>
         </StyledHomeSwipe>
     )
 }
