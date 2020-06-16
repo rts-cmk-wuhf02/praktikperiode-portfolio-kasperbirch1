27b5fd7f28a8ef33121dce226fff1737f4ea034b
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-16 14:25:03 +0200

    updated

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,21 @@
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
-import React from "react"
 import SEO from "../components/seo"
 
 const SecondPage = () => {
+  const [currentHeight, setCurrentHeight] = useState(0);
+  useEffect(() => {
+    let NewHeight = window.innerHeight
+    setCurrentHeight(NewHeight)
+  }, []);
 
 
   return (
     <>
       <SEO title="Page two" />
-      <section style={{ height: `calc(${window.innerHeight} - 6rem)` }}>
+      <section style={{ height: `calc(${currentHeight}px - 6rem)` }}>
         <h1>Hi from the second page</h1>
+        {currentHeight}
         <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
       </section>
