3456a802cf2f7347b2cb7ec24d76c42597b4be2a
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-16 08:46:21 +0200

    updated

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import React from "react"
 import Header from "./header"
 import Footer from "./Footer"
-import Div100vh from 'react-div-100vh';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,11 +30,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <Div100vh style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+    <>
       <Header siteTitle={data.site.siteMetadata.title} menuLinks={data.site.siteMetadata.menuLinks} />
       <main style={{ backgroundImage: 'url("https://images.pexels.com/photos/459654/pexels-photo-459654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>{children}</main>
       <Footer siteSlogan={data.site.siteMetadata.slogan} />
-    </Div100vh>
+    </>
   )
 }
 
