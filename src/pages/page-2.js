075f163003eab8c06e8e0a58ce5b5b06553ba007
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-17 09:44:19 +0200

    updated

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,25 +1,21 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
 
 const SecondPage = () => {
-  const [currentHeight, setCurrentHeight] = useState(0);
-
   useEffect(() => {
     window.addEventListener('resize', () => {
-      document.querySelector(':root').style.setProperty('--vh', window.innerHeight / 100 + 'px');
-      setCurrentHeight(window.innerHeight / 100 + 'px')
+      document.querySelector(':root').style.setProperty('--vh', window.innerHeight + 'px');
     })
 
   }, []);
   return (
     <>
       <SEO title="Page two" />
-      <section style={{ height: 'calc((100 * var(--vh)) - 6rem)' }}>
+      <section style={{ height: 'calc(var(--vh) - 6rem)' }}>
         <h1>Hi from the second page</h1>
         <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
-        <h2 style={{ fontSize: '3rem', color: 'red', textAlign: 'center' }}>{`InnerHeight: ${currentHeight}`}</h2>
       </section>
     </>
   )
