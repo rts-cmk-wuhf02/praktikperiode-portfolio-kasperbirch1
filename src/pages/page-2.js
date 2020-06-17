68cb9d8461dd937c14fa3898f1ec837e0e4b2517
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-17 11:33:01 +0200

    updated

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -30,7 +30,7 @@ const SecondPage = () => {
   return (
     <>
       <SEO title="Page two" />
-      <section style={{ height: 'var(--vh)' }}>
+      <section style={{ height: 'calc(var(--vh) - 6rem)' }}>
         <h1>Hi from the second page</h1>
         <Link to="/">Go back to the homepage</Link>
         <p>Welcome to page 2</p>
