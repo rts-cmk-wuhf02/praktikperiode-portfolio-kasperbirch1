38167f5e168f303e2297575a5340d789247f1909
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-17 11:36:23 +0200

    updated

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,11 +7,19 @@
 
 import { graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
 import Header from "./header"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    // override root variable called --vh width current screen size to and listen for a resize event
+    document.querySelector(':root').style.setProperty('--vh', window.innerHeight + 'px');
+    window.addEventListener('resize', () => {
+      document.querySelector(':root').style.setProperty('--vh', window.innerHeight + 'px');
+    })
+
+  }, []);
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react"
+import React from "react"
 import SEO from "../components/seo"
 import NestedSwipers from '../components/Hero/NestedSwipers'
 
 const IndexPage = () => {
-  useEffect(() => {
-    // override root variable called --vh width current screen size to and listen for a resize event
-    document.querySelector(':root').style.setProperty('--vh', window.innerHeight + 'px');
-    window.addEventListener('resize', () => {
-      document.querySelector(':root').style.setProperty('--vh', window.innerHeight + 'px');
-    })
 
-  }, []);
   return (
     <>
       <SEO title="Home" />
