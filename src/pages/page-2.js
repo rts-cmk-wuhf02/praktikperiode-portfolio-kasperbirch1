67b75d4c3c02ca3ebf95d48276c5cf662ee7bf90
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-17 09:02:29 +0200

    updated

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,46 +1,30 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
 import styled from 'styled-components'
 const StyledSection = styled.section`
     height: -webkit-fill-available;
     min-height: -webkit-fill-available;
+    h1 {
+      color: red;
+    }
 `
 
 const SecondPage = () => {
   return (
     <>
       <SEO title="Page two" />
-      <StyledSection>
-        <h1>Hi from the second page</h1>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
-        <h2 style={{ fontSize: '3rem', color: 'red', textAlign: 'center' }}>{`InnerHeight: ${currentHeight}`}</h2>
-      </StyledSection>
+      <section style={{ height: 'calc(100vh - 6rem)' }}>
+        <StyledSection>
+          <h1>Hi from the second page</h1>
+          <p>Welcome to page 2</p>
+          <Link to="/">Go back to the homepage</Link>
+          {/* <h2 style={{ fontSize: '3rem', color: 'red', textAlign: 'center' }}>{`InnerHeight: ${currentHeight}`}</h2> */}
+        </StyledSection>
+      </section>
     </>
   )
 }
 
 export default SecondPage
 
-
-/*
-const [currentHeight, setCurrentHeight] = useState(0);
-  useEffect(() => {
-    let NewHeight = window.innerHeight
-    window.addEventListener("orientationchange", function () {
-      if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
-        setCurrentHeight(NewHeight)
-      }
-    }, false);
-
-  }, [NewHeight]);
-
-
-      <section style={{ minHeight: `calc(${currentHeight}px - 6rem)` }}>
-
-      </section>
-
-
-
-*/
